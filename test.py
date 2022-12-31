e807ae3587ab3ca34bbd100e7c22e54ae37ab879
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-31 13:08:52 +0100

    datoteke

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("test")
+import pandas as pd
+import pickle as pic
+
+class UserItemData:
+    def __init__(self, path) -> None:
+        self.path = path
+        #self.from_date = from_date
+        #self.to_date = to_date
+        #self.min_ratings = min_ratings
+
+    def read(self):
+        df = pd.read_csv(self.path)
+        return df
+
+uim = UserItemData('data/user_ratedmovies.dat')
+print(uim.read())
