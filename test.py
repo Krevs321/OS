c34b84f8d4a37f7a04307dc1325d911d8a06dbba
Gasper Krevs <gasper.krevs@gmail.com>
2022-12-31 16:19:30 +0100

    neki

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ import pandas as pd
 import pickle as pic
 
 class UserItemData:
-    def __init__(self, path) -> None:
+    fileLines = []
+    def init(self, path, start_date = None, end_date = None, min_ratings = 0):
         self.path = path
-        #self.from_date = from_date
-        #self.to_date = to_date
-        #self.min_ratings = min_ratings
+        self.start_date = start_date
+        self.end_date = end_date
+        self.min_ratings = min_ratings
+        with open(path, 'r') as f:
+            self.fileLines = f.readlines()
 
-    def read(self):
-        df = pd.read_csv(self.path)
-        return df
+    def nratings(self):
+        return len(self.fileLines)
 
-uim = UserItemData('data/user_ratedmovies.dat')
-print(uim.read())
+uim = UserItemData('data/user_ratedmovies.dat', start_date = '12.1.2007', end_date='16.2.2008', min_ratings=100)
+print(uim.nratings())
